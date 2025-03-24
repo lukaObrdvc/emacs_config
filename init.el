2d670e8483b699b40a29cfa9d616866d70a49552
LukaObrdvc <obradovicc.lukaa@gmail.com>
2025-03-24 02:58:22 +0100

    cleaned up stuff, added some details

--- init.el
+++ init.el
@@ -2,49 +2,13 @@
 
 ;; BIGGIE DEAL: when adding env vars, add them to PATH variable, not just as a new variable............
 
-;; @TODO
 
-;; maybe jkl; for movement is not that bad (or vim)
-
-
-;; have to bind/unbind stuff for every shift version of key....
-;; same for alt, ctrl? (simialr?)
-;; always be in __MyMode when opening file/buffer??
-;; how to disable G recentering to top or bot??
-
-;; how to make line wraps not from start, but kind of indented??
 ;; maybe add smooth scrolling??
 ;; make buffer name in modeline stand out...
 ;; adding keywords with font-lock can do so much highlighting......
 ;; how to bold text that begins with @ ??
 ;; should probably add hl line...
 
-;; how can I make text (mainly in text based files) reformat entirely
-;; when I want to add words inside a paragraph and maintain some horizontal
-;; margin, I want to entire paragraph to reformat.....
-
-
-;; beginning-of-visual-line is decent with wrapping lines...??
-
-;; multiple cursors?
-;; C-{ will auto scope and stuff?
-;; auto reindent when yanking ??
-;; guard (automatic?)
-;; add horizontal whitespace up to point in a region
-;; make an emacs function that aligns the all = with the horizontally furthest one
-
-
-;; org           
-;; org-habit     
-;; org-journal   
-;; org-noter     
-;; org-pomodoro  
-;; org-recur     
-;; org-roam      
-;; org-superstar 
-;; org-tree-slide
-
-;; ledger
 ;; look for other stuff in themes...?
 
 
@@ -58,6 +22,12 @@
 (defvar code-file-extensions "*.h *.c")
 ;; add cd to default project here
 ;; split right as well by default
+;; can also add some files to open on startup..?
+
+;; (custom-set-faces
+;;  '(default ((t (:background "#E0D8D0")))))
+;; (custom-set-faces
+;;  '(fringe ((t (:background "#E0D8D0")))))
 
 
 (add-to-list 'load-path "w:/Emacs/packages")
@@ -65,7 +35,7 @@
 
 (add-to-list 'custom-theme-load-path "w:Emacs/themes")
 (load-theme 'moonrocks t)
-;;(load-theme 'desert-sapphire t)
+;; (load-theme 'desert-sapphire t)
 
 ;; (set-face-attribute 'default nil :font "JetBrains Mono Regular-11")
 ;; (set-face-attribute 'default nil :font "Inconsolata-g-11")
@@ -111,6 +81,8 @@
 (ido-everywhere t)
 (fido-mode t)
 
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
@@ -119,7 +91,6 @@
 
 (eval-after-load 'grep '(setq grep-use-null-device nil))
 
-;; do same for Ido Completions ...?
 (push '("\\*completions\\*"
         (display-buffer-use-some-window display-buffer-pop-up-window)
         (inhibit-same-window . t))
@@ -131,175 +102,25 @@
   :keymap __MyKeymap)
 
 
-;; @@ORG CONFIGS
+;; @@ORG
 
 (require 'org)
 (setq org-M-RET-may-split-line nil)
 (setq org-hide-emphasis-markers t)
 (setq org-hide-leading-stars t)
 (add-hook 'org-mode-hook 'org-indent-mode)
-;; (setq org-pretty-entities t) ;; this is iffy, because _ will put in down
 (setq org-hide-block-startup t)
-(setq insert-heading-respect-content t)
+(setq insert-heading-respect-content t) ;; this appears to not work...
 (setq org-log-done nil)
 (setq org-log-into-drawer nil)
-;; have to add agenda files, directory...
 (setq org-agenda-files (directory-files-recursively "w:Notes/" "\\.org$"))
+;; figure out if this works...
 (setq org-todo-keywords '((sequence "TODO(t)" "|" "CANCEL(c!)" "DONE(d!)")))
 (setq org-image-actual-width nil)
 (setq imagemagick-enabled-types t)
 (setq org-agenda-window-setup 'other-window)
 
-;; probably comment out these later...
-(setq org-use-tag-inheritance t)
-(setq org-capture-use-agenda-date nil)
-(setq org-capture-templates-contexts nil)
-(setq org-capture-window-setup 'other-window)
-
-;; (defun my/org-agenda-add-title ()
-;;   (when (and (string-match "\\.org$" (or buffer-file-name ""))
-;;              (not (org-before-first-heading-p)))
-;;     (let ((title (or (org-collect-keywords '("TITLE")) "Untitled")))
-;;       (insert (format "* %s\n" (cadr (assoc "TITLE" title)))))))
-
-;; (add-hook 'org-agenda-mode-hook #'my/org-agenda-add-title)
-
-;; (setq org-capture-templates
-;;       '(("n" "New Searchable Note" entry
-;;          (file (lambda () (concat "w:/Notes/" (format-time-string "%Y%m%d-%H%M%S") "-note.org")))
-;;          "* Overview\n  Created on: %U\n\n  %?"
-;;          :empty-lines 1)))
-
-;; auto adds to agenda files
-(defun my/org-add-to-agenda ()
-  (when (and buffer-file-name
-             (string-match-p (expand-file-name "w:/Notes/") buffer-file-name))
-    (unless (member buffer-file-name org-agenda-files)
-      (setq org-agenda-files (append org-agenda-files (list buffer-file-name))))))
-
-(add-hook 'after-save-hook 'my/org-add-to-agenda)
-
-
-;; (defun my/org-auto-add-parent-tags ()
-;;   "Ensure subtags inherit their parent tag."
-;;   (when-let ((tags (org-get-tags)))
-;;     (when (member "keto" tags)
-;;       (org-set-tags (cons "diet" tags)))
-;;     (when (member "paleo" tags)
-;;       (org-set-tags (cons "diet" tags)))
-;;     (when (member "cardio" tags)
-;;       (org-set-tags (cons "exercise" tags)))
-;;     (when (member "strength" tags)
-;;       (org-set-tags (cons "exercise" tags)))))
-
-
-
-;; how to show title in agenda view
-;; how to link to a file now?
-;; what about showing the inheritance hierarchy of tags?
-;; how to insert metadata
-;; how to open capture in other window
-
-;; datetime as filename always, means can't easily distuinguish
-;;   names of buffers (or files for that matter)
-;; can you append all files choosen to one file?
-
-
-
-
-;;; you basically want two types of notes, one type are in arena,
-;;; they are more temporary, you just create them quickly and put them
-;;; all there, and you make sure to tag them properly, the other ones
-;;; are more structured and long term and are more monolithic, so
-;;; you create them manually, and you can use the temporary notes
-;;; to filter them by tags and extract information from them and
-;;; put it into these more permanent notes
-
-;;; since knowledge is hierarchical, you always want to approach it
-;;; that way, if it happens that in the special case you see a
-;;; connection between these permanent notes, just make a note
-;;; about that connection, basically do linking between these [tag properly...]
-;;; files (or abstract it into a different file that links toward
-;;;  them), this way you can reason between connections, but also
-;;; this way these permanent notes will, and should never link
-;;; toward the more temporary notes; again what matters is what
-;;; is in your head, and you shouldn't need to link together
-;;; everything like a wiki, you should already know certain
-;;; concepts or know where they are in files and be able to
-;;; refresh your memory about them, this way you don't need
-;;; a thing like org-roam...
-
-;;; basically do your organization with tags, make them very simple
-;;; and don't fuck up with their use so that it's hard to reorganize
-;;; something, if you need more proper organization, hierarchy, then
-;;; do it inside the file, with org outlining
-
-;;; basically if you take a note about some deep thought
-;;; about some subject, or connection, or insight, or deep undestanding
-;;; or whatever, or similar stuff like that, where you just want to
-;;; note it down and not worry about the structure initially as you
-;;; continue to think about it more, and process it, then you just
-;;; note it down, and tag it properly that's all it takes, proper usage
-;;; of tags, [in fact the same way you used the canvas or notable or
-;;;   notepad, you just either create a note {and then never find it again kind of...}, or you pick a canvas
-;;;   and pick a place there and just note something down and that's it
-;;;   it's not even well organized and doesn't look that good, but it's
-;;;   in a proper place, where you can find it again and to what it's
-;;;   associated with and you can then restructure all of that information
-;;;   later], in order to associate a note with other things and find it
-;;; later and restructure all of the information over time...
-;;; or also do restructuring inside the file with org markup and stuffs...
-
-;;; have a special file that deals with tags and their hierarchies...
-
-;;; maybe figure out some directory stuff, andor filenames..
-;;;  dailies like worklogging, ?templates, .. .  .
-
-;;; but then you kind of just like figure out the tags !
-
-;;; also you can actually view pdfs in DocView bruh...
-
-;; look up hyprelinks guide org, cream guy lul...
-
-;; what happens when you change tags???
-
-
-;; (setq org-tag-alist '((:startgroup . nil)
-                      ;; ("general" . ?g)
-                        ;; ("work" . ?w)
-                        ;; ("emacs" . ?e)
-                      ;; (:endgroup . nil)))
-
-;; (setq org-tag-alist '((:startgroup . nil)
-;;                       ("diet" . ?d)   ;; Main category
-;;                         ("keto" . ?k)  ;; Subcategory of diet
-;;                         ("paleo" . ?p)
-;;                       (:endgroup . nil)
-;;                       ("exercise" . ?e)   ;; Another category
-;;                         ("cardio" . ?c)
-;;                         ("strength" . ?s)))
-
-
-;; (setq org-list-automatic-rules '((newline . t)))
-;; (setq org-list-automatic-rules '((newline . t) (space . t)))
-;; (setq org-adapt-indentation nil)
-;; (setq org-list-indent-offset 0)
-
-
-;; I mean there's mainly this:
-;; linking, tagging, searching, creating, modifying
-;; and you need to make these convenient basically....
-
-
-
-
-
-
-
 ;; @COMMANDS
-
-;; rename certain commands to prefix with :  , also pascal case?
-
 
 (defun __update-cursor ()
   (setq cursor-type (if __MyMode 'box 'bar))
@@ -344,6 +165,7 @@
   )
 
 ;; doesn't close org stuff..?
+;; also doesn't work on dired
 (defun __clean-system-buffers ()
   (interactive)
   (dolist (buf (buffer-list))
@@ -356,7 +178,7 @@
   (interactive)
   (save-some-buffers t)
   )
-(run-with-timer 0 30 '__save-all)
+(run-with-timer 0 30 '__save-all) ;; is this stopping to work at some points?????
 
 (defun __init-project-name (arg1)
   (interactive "sChoose project name: ")
@@ -475,6 +297,15 @@
   (cd (file-name-directory buffer-file-name))
   )
 
+(defun __ripgrep-c-functions ()
+  (interactive)
+  (__save-all)
+  (if (= (count-windows) 1) (split-window-right))
+  (cd (concat "w:/Projects/" project-name "/"))
+  (grep "rg --line-number --ignore-case --regexp=\"^[[:space:]]*(inline|static|extern)?[[:space:]]*[a-zA-Z_][a-zA-Z0-9_]*[[:space:]]+[a-zA-Z_][a-zA-Z0-9_]*[[:space:]]*\\([^)]*\\)[[:space:]]*\\{?$\" --glob=\"*.c\" --glob=\"*.h\" .")
+  (cd (file-name-directory buffer-file-name))
+  )
+
 (defun __grep-current-buffer-only (arg1)
   (interactive "sgrep findstr: ")
   (__save-all)
@@ -521,6 +352,7 @@
 
 (defun :note-rename (arg1)
   (interactive "sNew note title: ")
+  (__save-all)
   (let* ((Dir (file-name-directory buffer-file-name))
          (NewName (concat (substring (file-name-base buffer-file-name) 0 14) "_" arg1 ".org"))
          (NewPath (concat Dir NewName))
@@ -530,7 +362,7 @@
     (find-file NewPath)
     (save-excursion
       (goto-char (point-min))
-      (kill-line)
+      (delete-region (line-beginning-position) (line-end-position))
       (insert "#+TITLE: " arg1)
       )
     (save-buffer)
@@ -539,9 +371,11 @@
 
 (defun :note-tagfile ()
   (interactive)
-  (find-file "w:/Notes/TAGS.org")
+  (find-file "w:/Notes/tags.org")
   )
 
+;; perhaps add one that simply lists all files by searching $: ...
+;; actually using :note-tagsearch with no input does that...
 (defun :note-ripgrep (arg1)
   (interactive "sripgrep notes: ")
   (__save-all)
@@ -557,11 +391,6 @@
   (if (= (count-windows) 1) (split-window-right))
   (cd "w:/Notes/")
   
-  ;; (grep (format "rg --pcre2 \"#\\+FILETAGS:(?!.*\\b(?:%s)\\b)(?=.*\\b(?:%s)\\b)%s\" --line-number --ignore-case --glob=\"*.org\" ."
-  ;;               "tag1|tag2|tag9"
-  ;;               "tag3|tag4|tag8"
-  ;;               "(?=.*\\btag5\\b)(?=.*\\btag6\\b)(?=.*\\btag7\\b)"))
-
   ;; Extract NOT, OR, AND tags using regex
   (let* ((not-tags (if (string-match "~\\([^+|~]+\\)" query)
                        (match-string 1 query) ""))
@@ -569,37 +398,26 @@
                        (match-string 1 query) ""))
          (and-tags (if (string-match "+\\([^+|~]+\\)" query)
                        (match-string 1 query) ""))
-
+         
          ;; Convert extracted tags into the correct ripgrep format
          (not-part (if (string-empty-p not-tags) ""
                      (format "(?!.*\\b(?:%s)\\b)" 
                              (replace-regexp-in-string "," "|" not-tags))))
-
          (or-part (if (string-empty-p or-tags) ""
                     (format "(?=.*\\b(?:%s)\\b)" 
                             (replace-regexp-in-string "," "|" or-tags))))
-
          (and-part (if (string-empty-p and-tags) ""
                      (mapconcat (lambda (tag) (format "(?=.*\\b%s\\b)" tag))
                                 (split-string and-tags ",") "")))
 
-         ;; Construct final grep command    #\\+FILETAGS
          (rg-command (format "rg --pcre2 \"\\$:%s%s%s\" --line-number --ignore-case --glob=\"*.org\" ."
                              not-part or-part and-part)))
-
-    ;; Execute grep with constructed query
-    (grep rg-command))
-  
+    (grep rg-command)
+    )
   (cd (file-name-directory buffer-file-name))
   )
 
-
-;; how do I create a special function that will rename this file after I've done some writing in it, so that the date and time identifier remains the same, and I will also want to create a function that for the file removes the tags from the file name, and appends new tags from the second line of the file that begins with #+FILETAGS: , 
-
 ;; @KEYBINDINGS
-
-
-;; unbind in modal for shift + letter ??
 
 (global-set-key (kbd "<f19>") #'__ToggleCommand) ;; this is caps lock, use Windows Power Tools to remap to f19
 (global-set-key (kbd "<tab>") 'dabbrev-expand)
@@ -634,7 +452,7 @@
 (define-key __MyKeymap (kbd "r") 'kill-line)
 (define-key __MyKeymap (kbd "R") '__duplicate-line)
 (define-key __MyKeymap (kbd "g") 'recenter-top-bottom)
-;;(define-key __MyKeymap (kbd "g") 'keyboard-quit)
+;; (define-key __MyKeymap (kbd "/") 'keyboard-quit) (this is bad because you can only use it on set-mark, so you might as well use C-g for that too...)
 (define-key __MyKeymap (kbd "b") (kbd "C-u C-SPC")) ;; pop mark
 (define-key __MyKeymap (kbd "B") 'pop-global-mark)
 (define-key __MyKeymap (kbd "<tab>") 'indent-for-tab-command)
@@ -647,12 +465,15 @@
 (define-key __MyKeymap (kbd "h") 'switch-to-buffer)
 (define-key __MyKeymap (kbd "H") #'(lambda () (interactive) (switch-to-buffer (other-buffer))))
 (define-key __MyKeymap (kbd "y") 'find-file)
+(define-key __MyKeymap (kbd "Y") 'project-find-file)
 (define-key __MyKeymap (kbd "o") 'imenu)
 (define-key __MyKeymap (kbd ";") 'comment-line)
 (define-key __MyKeymap (kbd "n") 'next-error)
 (define-key __MyKeymap (kbd "N") 'previous-error)
 (define-key __MyKeymap (kbd "`") '__clean-system-buffers)
 (define-key __MyKeymap (kbd "=") 'quick-calc)
+(define-key __MyKeymap (kbd "0") 'beginning-of-defun) ;; maybe change these..?
+(define-key __MyKeymap (kbd "-") 'end-of-defun) ;; you ought to not use them that much anyway........
 
 (define-key __MyKeymap (kbd "x c") '__compile)
 
@@ -669,11 +490,18 @@
 (define-key __MyKeymap (kbd "x m s") 'scratch-buffer)
 (define-key __MyKeymap (kbd "x m a") '__save-all)
 (define-key __MyKeymap (kbd "x m o") 'occur)
+(define-key __MyKeymap (kbd "x m d") 'dired)
+(define-key __MyKeymap (kbd "x m b") 'kill-buffer)
 
-(define-key __MyKeymap (kbd "x e m") 'query-replace)
+(define-key __MyKeymap (kbd "x e q") 'query-replace)
 (define-key __MyKeymap (kbd "x e i") 'beginning-of-buffer)
 (define-key __MyKeymap (kbd "x e k") 'end-of-buffer)
 (define-key __MyKeymap (kbd "x e h") 'delete-horizontal-space)
+(define-key __MyKeymap (kbd "x e j") 'backward-list) ;; maybe make these two regular keybindings?
+(define-key __MyKeymap (kbd "x e l") 'forward-list) ;; there's also backward-up-list, down-list
+(define-key __MyKeymap (kbd "x e p") 'insert-parentheses)
+(define-key __MyKeymap (kbd "x e u") 'delete-pair)
+(define-key __MyKeymap (kbd "x e g") 'goto-line)
 
 (define-key __MyKeymap (kbd "x w d") 'split-window-below)
 ;; (define-key __MyKeymap (kbd "x w r") 'split-window-right) ;; actually don't need this just use the lambda one instead
@@ -702,6 +530,18 @@
 (define-key __MyKeymap (kbd "x n g") ':note-ripgrep)
 (define-key __MyKeymap (kbd "x n s") ':note-tagsearch)
 
+(define-key __MyKeymap (kbd "x o c") 'org-cycle)
+(define-key __MyKeymap (kbd "x o g") 'org-cycle-global)
+(define-key __MyKeymap (kbd "x o i") 'org-display-inline-images)
+(define-key __MyKeymap (kbd "x o r") 'org-remove-inline-images)
+(define-key __MyKeymap (kbd "x o l") 'org-latex-preview)
+(define-key __MyKeymap (kbd "x o o") 'org-open-at-point)
+
+;; if you use this on a non-grep buffer, do M-x read-only-mode
+;;(global-set-key (kbd "C-S-w") 'wgrep-finish-edit) ;; saves wgrep changes
+;;(global-set-key (kbd "C-w") (kbd "C-c C-p")) ;; turns wgrep on
+
+
 ;; run project...
 
 ;; delete-file
@@ -711,47 +551,16 @@
 ;; delete-directory
 ;; ido-list-directory
 
-
-;; if you use this on a non-grep buffer, do M-x read-only-mode
-;;(global-set-key (kbd "C-S-w") 'wgrep-finish-edit) ;; saves wgrep changes
-;;(global-set-key (kbd "C-w") (kbd "C-c C-p")) ;; turns wgrep on
+;; @INFO
 
 ;; f3 - start macro
 ;; f4 - end macro, replay last macro
 
-
-;;(define-key c-mode-map (kbd "M-e") nil)
-
-
-
-;; @PREVIOUS KEYBINDINGS
-
-;;(global-set-key (kbd "C-M-f") 'project-find-file)
-;;(global-set-key (kbd "M-t") 'pop-global-mark)
-;;(global-set-key (kbd "C-<prior>") 'beginning-of-defun)
-;;(global-set-key (kbd "C-<next>") 'end-of-defun)
-;;(global-set-key (kbd "C-M-<left>") 'backward-list)
-;;(global-set-key (kbd "C-M-<right>") 'forward-list)
-;;(global-set-key (kbd "C-M-<up>") 'backward-up-list)
-;;(global-set-key (kbd "C-M-<down>") 'down-list)
-;;(global-set-key (kbd "<home>") 'insert-parentheses)
-;;(global-set-key (kbd "<end>") 'delete-pair)
-
-
-;; @INFO
-
-
-;; flyspell-mode .................
-;; upcase-region
 ;; ? on command in minibuffer to show possible completions
-;; SPC for M-f autocompletes
+;; SPC for find-file autocompletes
 ;; you can change font size with mouse wheel, and in the buffer border you can see offset from original
-;; ???? ~ back-to-indentation
-;; M-g g ~ goto-line
-;; occur-rename-buffer is basically wgrep
 ;; can press Q on a lot of emacs' buffers to close them instantly
 
-;; from old __save-all (although the let and inhibit-message doesn't work...)
-;; (setq current-prefix-arg '(!))
-;; (let ((inhibit-message t)) (call-interactively 'save-some-buffers))
+
+
 
